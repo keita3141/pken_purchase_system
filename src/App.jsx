977211f3848281c5c9da747e86af5974e46db0f6
hiratsuka <keipon1013hmas@icloud.com>
2026-03-14 17:20:56 +0900

    0314‐15

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <Header />
         {/* 開発環境でのみデバッグログを表示 */}
         <DebugLogger />
-        <main style={{ paddingTop: '3.5rem' }}>
+        <main style={{ paddingTop: 'var(--global-header-height)' }}>
           <Routes>
           {/* 認証が必要なルート */}
           <Route
